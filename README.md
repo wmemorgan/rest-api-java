c39619da6096f18c904d09008d51d084b1122dcc
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-07-31 15:31:55 +0800

    Initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+# Java REST API
---
+++ api/src/test/java/com/wilfredmorgan/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilfredmorgan.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
